1aa8910e121b17be7a1b9d11b64d99f6102eee8c
AnythingFree <93827376+AnythingFree@users.noreply.github.com>
2023-04-09 02:26:27 +0200

    Update novosti_mat.py

--- novosti_mat.py
+++ novosti_mat.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 import time
+import os
 
 # website url to monitor
 url = 'https://matinf.pmf.unibl.org/%d0%bd%d0%be%d0%b2%d0%be%d1%81%d1%82%d0%b8/'
@@ -29,9 +30,12 @@ with smtplib.SMTP(smtp_server, smtp_port) as server:
     for recipient in recipients_emails:
         server.sendmail(email, recipient, body)
 """
+if not os.path.exists("current_content.txt"):
+    with open(path, 'w'): 
+        
         
 # read previous website content from file
-with open("/etc/secrets/current_content.txt", 'r', encoding='utf-8') as f:
+with open("current_content.txt", 'r', encoding='utf-8') as f:
     previous_content = f.read()
 """
 while True:
@@ -45,7 +49,7 @@ current_content = [li.text for li in li_elements]
 # check for changes
 if str(current_content) != previous_content:
 
-    with open("/etc/secrets/current_content.txt", 'w', encoding='utf-8') as f:
+    with open("current_content.txt", 'w', encoding='utf-8') as f:
         f.write(str(current_content))
 
 
